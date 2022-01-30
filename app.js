486d251fde5cc928754e53d50f394da8b3675f1a
Shouryan Nikam <dev.shouryan@gmail.com>
2022-01-30 12:38:10 -0500

    usability improvements

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 function newItem() {
-	var item = document.getElementById('input').value;
+    var input = document.getElementById('input');
+    input.focus();
+    input.select();
+	var item = input.value;
 	var ul = document.getElementById("list");
 	var li = document.createElement('li');
   li.appendChild(document.createTextNode("- "+item));
   ul.appendChild(li);
-  document.getElementById('input').value="";
+  input.value="";
   li.onclick = removeItem;
 }
 
