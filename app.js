3cd1a50e3eadcde78d62bb84c40e2305bcf8ae1b
Shouryan Nikam <dev.shouryan@gmail.com>
2022-10-13 16:36:08 -0400

    fixed localstorage and save theme info

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
+window.onload = loadTasks;
+
+var intervalId = window.setInterval(function(){
+    var input = document.getElementById('input');
+    input.focus()
+}, 500);
+
+function loadTasks() {
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    var ul = document.getElementById("list");
+    tasks.forEach(task => {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode("- "+task));
+        ul.appendChild(li);
+        li.onclick = removeItem;
+    });
+
+    var theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+        restoreTheme();
+    }
+  
+}
+
 function newItem() {
     var input = document.getElementById('input');
-	var item = input.value;
-	var ul = document.getElementById("list");
-	var li = document.createElement('li');
+    var item = input.value;
+    if (item === "" || item === " ") {
+        return;
+    }
+    var ul = document.getElementById("list");
+
+    
+    var li = document.createElement('li')
+    var tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+    tasks.push(item);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+
   li.appendChild(document.createTextNode("- "+item));
   ul.appendChild(li);
+
   input.value="";
   li.onclick = removeItem;
 }
@@ -16,19 +51,33 @@ document.body.onkeyup = function(e){
   }
 
 function removeItem(e) {
-  e.target.parentElement.removeChild(e.target);
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+    tasks.forEach(element => {
+        if (element === e.target.innerHTML.slice(2)) {
+            tasks.splice(tasks.indexOf(element), 1);
+        }
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    e.target.parentElement.removeChild(e.target);
 }
 
-function toggleDarkMode() {
-  var head = document.getElementById("input")
-  var body = document.getElementById("body")
-  input.classList.toggle("dark-mode");
-  body.classList.toggle("dark-mode");
+function toggleTheme() {
+    var theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+        localStorage.setItem("theme", "light");
+    } else {
+        localStorage.setItem("theme", "dark");
+    }
+    var head = document.getElementById("input")
+    var body = document.getElementById("body")
+    input.classList.toggle("dark-mode");
+    body.classList.toggle("dark-mode");
 }
 
-function toggleLightMode() {
-  var head = document.getElementById("input")
-  var body = document.getElementById("body")
-  input.classList.toggle("light-mode");
-  body.classList.toggle("light-mode");
+function restoreTheme() {
+    var head = document.getElementById("input")
+    var body = document.getElementById("body")
+    input.classList.toggle("dark-mode");
+    body.classList.toggle("dark-mode");
 }
